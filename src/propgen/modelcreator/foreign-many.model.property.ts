efd52ec0b730f34d1e4a54756ddc2f263f59f7eb
Christian Stroehmeier <stroemi@uni-paderborn.de>
2018-01-16 19:12:06 +0100

    Ensure that this relation is always present
    Should only affect newly created entities

--- src/propgen/modelcreator/foreign-many.model.property.ts
+++ src/propgen/modelcreator/foreign-many.model.property.ts
@@ -3,6 +3,7 @@ import {ModelProperty} from './model.property';
 export class ForeignManyModelProperty extends ModelProperty<Array<number>> {
   public service: any;
   public type: any;
+  public defaultValue: Array<number> = [];
   isValid(value: Array<number>): boolean {
     return true;
   }
