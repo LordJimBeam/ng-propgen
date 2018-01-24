99d28c02487a518fb7265e51ca9fb28acddc0b79
Christian Stroehmeier <stroemi@uni-paderborn.de>
2018-01-24 02:20:05 +0100

    Decorators gonna decorate

--- src/propgen/modelcreator/text.model.property.ts
+++ src/propgen/modelcreator/text.model.property.ts
@@ -2,7 +2,7 @@ import {ModelProperty} from './base/model.property';
 import {DefaultComponent} from '../decorators/default-component.decorator';
 import {TextFormComponent} from './formcomponents/text.form.component';
 
-DefaultComponent(TextFormComponent)
+@DefaultComponent(TextFormComponent)
 export class TextModelProperty extends ModelProperty<string> {
   public constructor(init?: Partial<TextModelProperty>) {
     super();
